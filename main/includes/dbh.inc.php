1973990d3fd3a42ac858356f850b915fb360a969
Tacs25 <90818953+Tacs25@users.noreply.github.com>
2022-02-11 14:00:11 +0800

    Update dbh.inc.php

--- main/includes/dbh.inc.php
+++ main/includes/dbh.inc.php
@@ -1,9 +1,15 @@
 <?php
 
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'access';
+// development 
+// $servername = 'localhost';
+// $username = 'root';
+// $password = '';
+// $dbname = 'access';
+
+$servername = 'remotemysql.com';
+$username = '6VJZxCdZXy';
+$password = 'm4efKGyzN6';
+$dbname = '6VJZxCdZXy';
 
 $conn = mysqli_connect($servername, $username, $password , $dbname);
 
